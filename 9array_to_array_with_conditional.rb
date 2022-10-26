74e3654de70249559513813afc52bce4aa9aef77
Dan Cross <dancross96@gmail.com>
2022-10-25 23:50:36 -0500

    last update for today

--- 9array_to_array_with_conditional.rb
+++ 9array_to_array_with_conditional.rb
@@ -309,3 +309,6 @@
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+# numbers = [2, 4, 5, 1, 8, 9, 7]
+# p numbers.select {|number| number.odd?}
